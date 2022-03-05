6b09585d8024772b21bc0cd4a87c409a46b8d75b
brycklen <48401679+brycklen@users.noreply.github.com>
2022-03-04 21:30:34 -0700

    Delete brycklenvue directory

--- brycklenvue/brycklenvue/src/main/java/brycklen/project/brycklenvue/repository/CourseRegistrationRepository.java
+++
@@ -1,7 +0,0 @@
-package brycklen.project.brycklenvue.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import brycklen.project.brycklenvue.entity.Course;
-import brycklen.project.brycklenvue.entity.Student;
-
-public interface CourseRegistrationRepository extends JpaRepository<Course, Student> {}
--- brycklenvue/brycklenvue/src/main/java/brycklen/project/brycklenvue/repository/CourseRepository.java
+++
@@ -1,6 +0,0 @@
-package brycklen.project.brycklenvue.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import brycklen.project.brycklenvue.entity.Course;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {}
